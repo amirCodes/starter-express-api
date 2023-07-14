c5728c6ff8b78517bca92c96a4bc672930267f7d
ideraslab <ideras@123>
2023-07-14 15:16:34 +0800

    changed setting for cors

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -6,10 +6,13 @@ const generateToken = (res, userId) => {
     });
 
     res.cookie('jwt', token, {
-        httpOnly: false,
+        // httpOnly: false,
         secure: process.env.NODE_ENV !== 'development', // Use secure cookies in production
-        sameSite: 'strict', // Prevent CSRF attacks
+        // sameSite: 'strict', // Prevent CSRF attacks
         // maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+        httpOnly: false,
+        signed: true,
+        sameSite: 'none',
         maxAge: 1 * 60 * 60 * 1000, // 30 days
     });
 };
